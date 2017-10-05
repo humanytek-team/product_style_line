07dd6418f1c7c7aca5433edddd805386cfac11d6
Manuel Marquez <buzondemam@gmail.com>
2017-10-05 10:45:55 -0400

    [ADD] Adds manager of product style lines.

---
+++ __openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Humanytek - Manuel Marquez <manuel@humanytek.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    'name': 'Product Style Lines Manager',
+    'version': '9.0.0.1.0',
+    'category': 'Product',
+    'summary': 'Product Style Lines Manager',
+    'author': 'Humanytek',
+    'website': "http://www.humanytek.com",
+    'license': 'AGPL-3',
+    'depends': ['product'],
+    'data': [
+        'views/product_style_line_view.xml',
+        'security/ir.model.access.csv'
+    ],
+    'installable': True,
+    'auto_install': False
+}
